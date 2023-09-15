33450b3b12cdcef559ef64cd84931aac39b72c17
Alfred Louis <alfredlouisw8@gmail.com>
2023-09-15 23:38:37 +0700

    fix deployment

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'mrwhqxpb',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
